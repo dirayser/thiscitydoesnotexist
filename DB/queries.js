7a05cc4763b757c5eb8ad66379bd0e7ca17fcebd
Dmitriy Dmitriyev <45948880+dirayser@users.noreply.github.com>
2020-12-21 20:35:08 +0200

    Add queries

--- DB/queries.js
+++ DB/queries.js
@@ -17,6 +17,7 @@ const pool = new Pool({
   ssl: true
 });
 
+// Запрос в таблицу улиц
 
 pool.query('SELECT street_id, name FROM public.streets', (err, res) => {
   if(err) {
@@ -30,8 +31,65 @@ pool.query('SELECT street_id, name FROM public.streets', (err, res) => {
   pool.end();
 });
 
+// Запрос в таблицу вакансий
+
+pool.query('SELECT institution_id, gender FROM public.vacancies', (err, res) => {
+  if(err) {
+    console.error(err)
+  }
+  else {
+    const answer = res.rows;
+    console.log(answer)
+  }
+  
+  pool.end();
+});
+
+// Запрос в таблицу заявок
+
+pool.query('SELECT application_id, user_id FROM public.applications', (err, res) => {
+  if(err) {
+    console.error(err)
+  }
+  else {
+    const answer = res.rows;
+    console.log(answer)
+  }
+  
+  pool.end();
+});
+
+// Запрос в таблицу домов
+
+pool.query('SELECT street_id, number FROM public.buildings', (err, res) => {
+  if(err) {
+    console.error(err)
+  }
+  else {
+    const answer = res.rows;
+    console.log(answer)
+  }
+  
+  pool.end();
+});
+
+// Запрос в таблицу пользователей
 
 pool.query('SELECT user_id, name, surname, gender, birthday FROM public.users WHERE mode=\'Admin\'', (err, res) => {
+  if(err) {
+    console.error(err)
+  }
+  else {
+    const answer = res.rows;
+    console.log(answer)
+  }
+  
+  pool.end();
+});
+
+// Запрос в таблицу домов
+
+pool.query('SELECT building_id, type FROM public.buildings', (err, res) => {
   if(err) {
     console.error(err)
   }
